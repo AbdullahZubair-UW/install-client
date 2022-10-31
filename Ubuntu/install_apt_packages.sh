b01f7523f2d6463de8564da1d6931eb56e80bb0b
Abdullah Zubair <abdzub@hotmail.com>
2022-10-31 00:32:28 -0400

    normal edits

--- Ubuntu/install_apt_packages.sh
+++ Ubuntu/install_apt_packages.sh
@@ -8,13 +8,7 @@ MAGENTA="\033[35m"
 WHITE="\033[37m"
 NORMAL="\033[0;39m"
 
-sudo apt install zsh snap automake pkg-config gettext exa curl fbreader feh fuse fontconfig gawk git gnumeric gnupg2 \
-graphviz htop i3 i3lock imagemagick isync jq libnotify-bin libpq-dev libsqlite3-dev kitty make cmake cmatrix clang cpp coq mlocate \
-mpv mu4e npm opam openssl pandoc parallel pass pass-extension-otp pasystray pavucontrol picom postgresql pwgen python3 python3-gpg \
-python3-pip python3-dev pip polybar nodejs redshift restic ripgrep rlwrap rofi rsync ranger scrot shellcheck sqlite3 sqlite3-pcre stow sudo \
-tree unclutter unifont unrar unzip urlscan urlview vim wget xbindkeys xcape xdotool xfce4-clipman xsel zathura zathura-cb \
-default-jre texlive-full libcoq-mathcomp clangd ccls fab lr tmux fd-find lsd fabric fab build-essential vim-nox \
-mono-complete golang nodejs openjdk-17-jdk openjdk-17-jre npm
+sudo apt install $(cat packages)
 
 sudo apt-get install libtool-bin qtile
 
